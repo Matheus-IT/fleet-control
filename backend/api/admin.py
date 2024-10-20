ebe90327b044f56fecc3978a0e7d1a067ff8e798
matheus <matheuscosta.tech@gmail.com>
2024-10-20 15:32:07 -0300

    Setting up custom  user

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,95 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib import admin
+from api.models import User
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+    new_password1 = forms.CharField(
+        label="New password", widget=forms.PasswordInput, required=False
+    )
+    new_password2 = forms.CharField(
+        label="Confirm new password", widget=forms.PasswordInput, required=False
+    )
+
+    class Meta:
+        model = User
+        fields = ("email", "password", "is_active", "is_staff")
+
+    def clean_password(self):
+        return self.initial["password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password1 = cleaned_data.get("new_password1")
+        new_password2 = cleaned_data.get("new_password2")
+
+        if new_password1 or new_password2:
+            if new_password1 != new_password2:
+                raise forms.ValidationError("The two password fields must match.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        new_password = self.cleaned_data.get("new_password1")
+        if new_password:
+            user.set_password(new_password)
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ["email", "is_staff"]
+    list_filter = ["email", "is_staff", "is_active"]
+    fieldsets = (
+        (None, {"fields": ("email", "password", "new_password1", "new_password2")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
+
 ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
@@ -123,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "api.User"
