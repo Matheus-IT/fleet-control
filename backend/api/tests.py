af63c1afc93a14c7a67bf359a0446e0f3b090cda
matheus <matheuscosta.tech@gmail.com>
2024-10-20 16:44:22 -0300

    Basic tests for retrieving auth tokens

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from api.models import User
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+
+class TokenTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_obtain_access_and_refresh_token(self):
+        # given
+        user = User.objects.create_user(email="test@test.com", password="12345678")
+        payload = {
+            "email": "test@test.com",
+            "password": "12345678",
+        }
+        # when
+        res = self.client.post(reverse("token_obtain_pair"), payload)
+        # then
+        response_data = res.json()
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue("refresh" in response_data)
+        self.assertTrue("access" in response_data)
+
+    def test_should_obtain_new_access_using_refresh_token(self):
+        # given
+        user = User.objects.create_user(email="test@test.com", password="12345678")
+        # Create a refresh token for the user directly
+        refresh = RefreshToken.for_user(user)
+
+        # Prepare payload with the refresh token
+        payload = {"refresh": str(refresh)}
+        # when
+        res = self.client.post(reverse("token_refresh"), payload)
+        # then
+        response_data = res.json()
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue("access" in response_data)
