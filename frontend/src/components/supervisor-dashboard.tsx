80b31af163512764ebc4f66cfa7d528766755f33
matheus <matheuscosta.tech@gmail.com>
2024-11-06 23:04:24 -0300

    Basic logic for supervisor dashboard

--- frontend/src/components/supervisor-dashboard.tsx
+++ frontend/src/components/supervisor-dashboard.tsx
@@ -1,3 +1,29 @@
+import VehicleEntryTile from "@/components/vehicle-entry";
+import { useVehicleEntries } from "@/hooks/useVehicleEntries";
+import { Spinner } from "@nextui-org/react";
+
 export default function SupervisorDashboard() {
-  return <h1>Supervisor</h1>;
+  const { data, isPending } = useVehicleEntries();
+  return (
+    <main className="container mx-auto h-screen">
+      <div className="flex flex-col gap-4 max-sm:p-4">
+        {data && data.length > 0 && (
+          <>
+            <h1 className="text-2xl text-center mt-6">Registros de veículos</h1>
+            {data.map((v) => (
+              <VehicleEntryTile key={v.licencePlate} vehicleEntry={v} />
+            ))}
+          </>
+        )}
+        {data && data.length == 0 && (
+          <h1 className="text-center text-lg">Nenhum registro de veículo.</h1>
+        )}
+        {isPending && (
+          <div className="h-screen flex items-center justify-center">
+            <Spinner size="lg" />
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
