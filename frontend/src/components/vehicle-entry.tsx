ba4f42494151e1eb050f4d76667a415d15d9200c
matheus <matheuscosta.tech@gmail.com>
2024-11-14 22:55:47 -0300

    Improving how things are displayed on the vehicle entry

--- frontend/src/components/vehicle-entry.tsx
+++ frontend/src/components/vehicle-entry.tsx
@@ -14,6 +14,14 @@ export default function VehicleEntryTile({
     return `${day}/${month}/${year}`;
   }
 
+  function formatTime(date: Date): string {
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
   return (
     <div className="w-full shadow-md rounded-lg p-4 border-l-5 border-l-green-700">
       <p className="text-lg">{vehicleEntry.vehicle.model}</p>
@@ -33,11 +41,18 @@ export default function VehicleEntryTile({
           {vehicleEntry.vehicle.is_at_workshop ? "sim" : "não"}
         </strong>
       </p>
-      <p className="text-base">Problema: {vehicleEntry.problem_reported}</p>
-      <p className="text-base">Oficina: {vehicleEntry.workshop.name}</p>
+      {vehicleEntry.vehicle.is_at_workshop && (
+        <>
+          <p className="text-base">Problema: {vehicleEntry.problem_reported}</p>
+          <p className="text-base">Oficina: {vehicleEntry.workshop.name}</p>
+        </>
+      )}
+
       <p className="text-sm">
-        Criado por <strong>{vehicleEntry.author.name}</strong> em{" "}
-        {formatDate(vehicleEntry.created_at)}
+        Ultima atualização em{" "}
+        <strong>{formatDate(vehicleEntry.created_at)}</strong> as{" "}
+        {formatTime(vehicleEntry.created_at)} por{" "}
+        <strong>{vehicleEntry.author.name}</strong>
       </p>
     </div>
   );
