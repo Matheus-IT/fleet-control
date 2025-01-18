cf3aad3c37b17b91262fa56760f15afcd13deaf9
matheus costa <matheuscosta.tech@gmail.com>
2025-01-18 22:39:52 +0000

    Adding pythonanywhere to allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG")
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "matheus2001.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
